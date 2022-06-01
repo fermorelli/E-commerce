c60d7d97881008bbfe65f4963fd713417e64d521
ilcosme <fer.c@live.com.ar>
2022-06-01 19:11:29 -0300

    added components, fetch to API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav';
+import Shop from './Shop';
+import About from './About';
+import ItemDetail from './ItemDetails';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+          <Routes>
+            {/* [//<Route path="/" exact element={<Home />}></Route>] */}
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" exact element={<Shop />} />
+            <Route path="/shop/:id" element={<ItemDetail />}/>
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/ItemDetails.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+
+
+function ItemDetail() {
+
+    const [item, setItem] = useState([]);
+
+    const fetchItem = async () => {};
+
+    useEffect(()=>{}, []);
+
+    
+
+    return (
+        <div className="App">
+            <h1>Item</h1>
+        </div>
+    );
+}
+
+export default ItemDetail;
