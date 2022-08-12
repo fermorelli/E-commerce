bf2756f42adc6b867b05b8aa609d3ca755cdda76
ilcosme <fer.cmorelli@gmail.com>
2022-08-12 17:08:57 -0300

    cart and shop page styling

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -13,18 +13,30 @@ const Cart = () => {
             {showCart && (
                 <>
                 <div className={styles.cartWrapper}>
-
-                </div>
-                <div>{cartItems.length === 0 ? <h4>Cart is empty</h4> : (
-                    <ul>
-                        {cartItems.map((item)=> (
-                            <CartItem key={item._id} item={item} />
-                        ))}
-                    </ul>
-                ) }</div>
-                <div className={styles.cartTotal}>
-                    <div></div>
-                    <div></div>
+                    <div style={{ textAlign: "right" }}>
+                        <i
+                            style={{ cursor: "pointer" }}
+                            className='fa-solid fa-x'
+                            aria-hidden='true'
+                            onClick={showHideCart}
+                            ></i>
+                    </div>
+                    <div className={styles.cartInnerWrapper}>
+                        {cartItems.length === 0 ? <h4>Cart is empty</h4> : (
+                        <ul>
+                            {cartItems.map((item)=> (
+                                <CartItem key={item._id} item={item} />
+                            ))}
+                        </ul>
+                        ) }
+                    </div>
+                    <div className={styles.cartTotal}>
+                        <div>Cart Total</div>
+                        <div></div>
+                        <div style={{ marginLeft: 5 }}>
+                            {cartItems.reduce((amount, item) => item.price + amount, 0)}
+                        </div>
+                    </div>
                 </div>
                 </>
             )}
