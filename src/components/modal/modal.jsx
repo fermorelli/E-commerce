0d7c43d40de477f4b01c8e0ad9570f1fbeea3200
ilcosme <fer.cmorelli@gmail.com>
2022-09-05 21:18:04 -0300

    initial hook forms implementation, installed joi

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,6 +1,13 @@
 import styles from './modal.module.css';
+import { useForm } from 'react-hook-form';
 
 export const Modal = ({ setIsOpen })=>{
+    const { register, handleSubmit, formState: { errors} } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
         <>
             <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
@@ -13,21 +20,39 @@ export const Modal = ({ setIsOpen })=>{
                             </button>
                         </div>
                         <div className={styles.modalBody}>
-                            <form action="" className={styles.modalForm}>
+                            <form onSubmit={handleSubmit(onSubmit)} action="" className={styles.modalForm}>
                                 <div className={styles.checkbox}>
-                                    <input type="checkbox" />
+                                    <input type="checkbox" {...register('credit', {
+                                        required: true
+                                    })}/>
                                     <label htmlFor="">Credit card</label>
-                                    <input type="checkbox" />
+                                    <input type="checkbox" {...register('debit', {
+                                        required: true
+                                    })}/>
                                     <label htmlFor="">Debit card</label>
                                 </div>
                                 <label htmlFor="">Card Number</label>
-                                <input type="text" />
+                                <input type="text" {...register('cardNumber', {
+                                        required: true,
+                                        maxLength: 16,
+                                        minLength: 16
+                                    })}/>
+                                    {errors.cardNumber?.type === 'required' && <span>Card Number is required</span>}
                                 <label htmlFor="">Expire date</label>
-                                <input type="text" />
+                                <input type="text" {...register('expireDate', {
+                                        required: true,
+                                        maxLength: 5,
+                                        minLength: 5
+                                    })}/>
+                                    {errors.expireDate?.type === 'required' && <span>Expire date  is required</span>}
                                 <label htmlFor="">Security code</label>
-                                <input type="text" />
+                                <input type="text" {...register('code', {
+                                        required: true,
+                                        maxLength: 3,
+                                        minLength: 3
+                                    })}/>
+                                <input type="submit" className={styles.button} value="pay" />
                             </form>
-                            <button className={styles.button}>pay</button>
                         </div>
                     </div>
                 </div>
